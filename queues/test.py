bd5c6c8cf557b02d044ef0b4c9d6c854b7474e7d
kris0801 <kristopherpaz@ufm.edu>
2023-02-15 16:59:15 -0600

    tarea

--- queues/test.py
+++ queues/test.py
@@ -1,3 +1,4 @@
+'''
 from queue import LinearQueue
 
 
@@ -36,3 +37,57 @@ queue.dequeue() # Queue Underflow (2nd scenario)
 queue_2 = LinearQueue(5)
 print(queue_2)
 queue.dequeue() # Queue Underflow (1st scenario)
+'''
+
+
+from tarea import Search
+key = Search()
+key.agregar("a", 1)
+key.agregar("b", 2)
+valor_a = key.buscar("a")
+valor_c = key.buscar("c")
+print(valor_a)  # imprime 1
+print(valor_c)  # imprime None
+'''
+
+'''
+from tarea import Stack
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print(stack.peek())  
+print(stack.pop())  
+print(stack.peek())  
+print(stack.pop())   
+print(stack.peek())  
+print(stack.pop())   
+print(stack.is_empty())  
+'''
+'''
+from tarea import CircularQueue
+q = CircularQueue(5)
+
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+
+print(q.enqueue(6))  
+
+print(q.dequeue())  
+print(q.dequeue())  
+
+q.enqueue(6)
+q.enqueue(7)
+q.enqueue(8)
+
+print(q.dequeue())  
+print(q.dequeue())
+print(q.dequeue()) 
+print(q.dequeue())  
+print(q.dequeue())  
+print(q.dequeue())
+print(q.dequeue())
